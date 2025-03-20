f6f276960aa3a9bcbc7ed3f9f9af9fb100e1999f
Yannis Yao <jenyunyao@gmail.com>
2025-03-20 15:56:26 +0800

    feat:新增数据类型的转换

--- 第二章/2-10-简单数据类型转换.py
+++ 第二章/2-10-简单数据类型转换.py
@@ -5,3 +5,24 @@ float(x)            将x转换成浮点型
 bool(x)             转换成bool类型 的True或 False
 str(x)              将x转换成字符串，适合人阅读
 '''
+
+number_1=111
+float_1=222.222233
+bool_1=''
+bool_2='NoNe'
+bool_3=' '
+string_1='234324'
+
+print(str(number_1))
+print(int(string_1),float(string_1))
+print(bool(bool_1),bool(bool_2),bool(bool_3))
+print(float(float_1),float(number_1))
+
+
+#  进制的转换
+print('进制的转换==========')
+s='10'
+print(int(s))
+print(int(s,2))  #二进制 逢二进一
+s2='1a'
+print(int(s2,16))
